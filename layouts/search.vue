ad8a7ebbd55feada939eb0323d47bbe9ac1d70ae
unknown <you@example.com>
2019-03-21 10:22:54 +0800

    meituan

---
+++ layouts/search.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="container">
+      <h1>11111111</h1>
+      <nuxt/>
+      <footer>footerfooterfooter</footer>
+   search
+  </section>
+</template> 
+
+
+
+
+
+<style scoped>
+.title {
+  margin: 50px 0;
+}
+
+</style>
---
+++ pages/search.vue
@@ -0,0 +1,35 @@
+<template>
+  <section class="container">
+   search
+   <ul>
+      <li v-for="(item,idx) in $store.state.navbar.app" :key="idx">{{item}}</li>
+   </ul>
+  </section>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  layout: 'search',
+  data() {
+    return {
+      list: []
+    }
+  },
+  async asyncData() {
+    let {status, data: {list}} = await axios.get('http://localhost:3000/city/list')
+    if (status === 200) {
+      return {
+        list: list
+      }
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+.title {
+  margin: 50px 0;
+}
+</style>
